454dfab25184c0e54a2ea29a7724fe2534959947
kjsbot <wang.fiona03@gmail.com>
2022-01-15 13:41:13 -0500

    some styling

--- client/src/components/Bookshelf.js
+++ client/src/components/Bookshelf.js
@@ -4,10 +4,26 @@ import '../images/search.png';
 import { Link } from "react-router-dom";
 import { motion } from 'framer-motion';
 
+// https://www.youtube.com/watch?v=1eO_hNYzaSc <- adding to shelf
+
 const Bookshelf = () => {
     return (
         <div className="shelf">
-            My Books
+
+            <div className="top">
+                <h1 className="book-h1">My Books</h1>
+
+                <div class="dropdown">
+                    <button class="dropbtn">Dropdown</button>
+                    <div class="dropdown-content">
+                        <a href="#">All</a>
+                        <a href="#">Not Started</a>
+                        <a href="#">Reading</a>
+                        <a href="#">Completed</a>
+                    </div>
+                </div>
+            </div>
+            
             <hr />
 
             <Link to="/search">
